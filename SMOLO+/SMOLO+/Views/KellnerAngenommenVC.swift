df4d7870df220ce47fd072a1d925b3f51fd3dc0f
Ibojaa <ibe2008@hotmail.de>
2019-11-15 15:30:47 +0100

    filter

--- SMOLO+/SMOLO+/Views/KellnerAngenommenVC.swift
+++ SMOLO+/SMOLO+/Views/KellnerAngenommenVC.swift
@@ -9,9 +9,16 @@
 import UIKit
 import Firebase
 
-class KellnerAngenommenVC: UIViewController, UITableViewDelegate, UITableViewDataSource, ExpandableHeaderViewDelegate {
+class KellnerAngenommenVC: UIViewController, UITableViewDelegate, UITableViewDataSource, ExpandableHeaderViewDelegate, DropDownBtnProtocoll {
+    
+    
     
     // VARS
+    
+    var showKat = [String()]
+    var DropDownKats = [String]()
+    var Button = DropDownBtn()
+    
     var Barname = String()
     var KellnerID = String()
     
@@ -41,6 +48,30 @@ class KellnerAngenommenVC: UIViewController, UITableViewDelegate, UITableViewDat
     
     
     // FUNCS
+    func passShowKat() {
+        showKat = Button.showKatBtn
+        print(showKat, "showkat")
+        reload()
+    }
+    
+    func getKategorien (){
+            var datref: DatabaseReference!
+            datref = Database.database().reference()
+
+        datref.child("Speisekarten").child("\(self.Barname)").observeSingleEvent(of: .value, with: { (snapshotKategorie) in
+            for key in (snapshotKategorie.children.allObjects as? [DataSnapshot])! {
+                print(key.key, "keykey")
+                        if !self.DropDownKats.contains(key.key) {
+                            self.DropDownKats.append(key.key)
+                        }
+                    }
+            self.Button.DropView.dropDownOpt = self.DropDownKats
+            print(self.DropDownKats, "dropdownkats")
+            self.Button.DropView.tableview.reloadData()
+
+        }, withCancel: nil)
+        
+        }
     
     
     func loadBestellungenKeys(){
@@ -60,6 +91,8 @@ class KellnerAngenommenVC: UIViewController, UITableViewDelegate, UITableViewDat
         }, withCancel: nil)
         
     }
+    
+
     func loadBestellungen(BestellungID: String){
         var datref: DatabaseReference!
         datref = Database.database().reference()
@@ -71,10 +104,12 @@ class KellnerAngenommenVC: UIViewController, UITableViewDelegate, UITableViewDat
                         let bestellungInfos = BestellungInfos(dictionary: dictionary)
                         self.Tischnummer.updateValue(bestellungInfos.tischnummer!, forKey: BestellungID)
                         self.FromUserID.updateValue(bestellungInfos.fromUserID!, forKey: BestellungID)
-                        self.TimeStamp.updateValue(bestellungInfos.timeStamp!, forKey: BestellungID) }} else {
+                        self.TimeStamp.updateValue(bestellungInfos.timeStamp!, forKey: BestellungID) }}
+                else if key.key == self.showKat {
                     let childsnapshotUnterkategorie = snapshot.childSnapshot(forPath: key.key)
                     if self.BestellungKategorien[BestellungID] != nil {
                         self.BestellungKategorien[BestellungID]?.append(key.key)
+                        
                         for children in (childsnapshotUnterkategorie.children.allObjects as? [DataSnapshot])! {
                             let childsnapshotItem = childsnapshotUnterkategorie.childSnapshot(forPath: children.key)
                             var x = self.BestellungUnterkategorien[BestellungID]
@@ -604,8 +639,23 @@ class KellnerAngenommenVC: UIViewController, UITableViewDelegate, UITableViewDat
     
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        getKategorien()
+        Button = DropDownBtn.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        Button.setTitle("Bereich", for: .normal)
+        showKat = "Shishas"
+        Button.translatesAutoresizingMaskIntoConstraints = false
+        Button.delegate = self
+
+        self.view.addSubview(Button)
+        
+        Button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        Button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        Button.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        Button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "hintergrund")!)
 
         loadBestellungenKeys()
@@ -654,3 +704,156 @@ class KellnerAngenommenVC: UIViewController, UITableViewDelegate, UITableViewDat
     
 }
 
+
+
+
+
+protocol DropDownProtocoll {
+    func DropDownPressed(array: [String])
+}
+protocol DropDownBtnProtocoll {
+    func passShowKat()
+}
+
+
+class DropDownBtn: UIButton, DropDownProtocoll {
+    func DropDownPressed(array: [String]) {
+        self.setTitle(array, for: .normal)
+        showKatBtn = array
+        self.delegate.passShowKat()
+        
+        self.dismissDropDown()
+    }
+    
+    
+    var DropView = DropDownView()
+    var showKatBtn = [String()]
+    var delegate: DropDownBtnProtocoll!
+    var height = NSLayoutConstraint()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.darkGray
+
+        DropView = DropDownView.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        DropView.delegate = self
+        DropView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    override func didMoveToSuperview() {
+        self.superview?.addSubview(DropView)
+        self.superview?.bringSubview(toFront: DropView)
+        
+        DropView.topAnchor.constraint(equalTo: self.topAnchor, constant: 40).isActive = true
+        DropView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        DropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        height = DropView.heightAnchor.constraint(equalToConstant: 0)
+    }
+    
+    var isOpen = false
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isOpen == false {
+            isOpen = true
+            NSLayoutConstraint.deactivate([self.height])
+            
+            if self.DropView.tableview.contentSize.height > 150 {
+                    self.height.constant = 150
+            } else {
+                self.height.constant = self.DropView.tableview.contentSize.height
+            }
+            
+            self.height.constant = 150
+            NSLayoutConstraint.activate([self.height])
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                self.DropView.layoutIfNeeded()
+                self.DropView.center.y += self.DropView.frame.height/2
+            }, completion: nil)
+        } else {
+            isOpen = false
+            NSLayoutConstraint.deactivate([self.height])
+            self.height.constant = 0
+            NSLayoutConstraint.activate([self.height])
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                self.DropView.center.y -= self.DropView.frame.height/2
+                self.DropView.layoutIfNeeded()
+            }, completion: nil)
+        }
+    }
+    
+    func dismissDropDown(){
+        isOpen = false
+        NSLayoutConstraint.deactivate([self.height])
+        self.height.constant = 0
+        NSLayoutConstraint.activate([self.height])
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            self.DropView.center.y -= self.DropView.frame.height/2
+            self.DropView.layoutIfNeeded()
+        }, completion: nil)
+         
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+
+class DropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
+    
+    var dropDownOpt = [String]()
+    
+    var tableview = UITableView()
+    
+    var delegate: DropDownProtocoll!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tableview.backgroundColor = UIColor.darkGray
+        self.backgroundColor = UIColor.darkGray
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(tableview)
+        
+        tableview.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableview.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableview.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableview.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dropDownOpt.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = dropDownOpt[indexPath.row]
+        cell.backgroundColor = UIColor.darkGray
+    
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.delegate.DropDownPressed(string: dropDownOpt[indexPath.row])
+        
+        
+    }
+    
+    
+}
